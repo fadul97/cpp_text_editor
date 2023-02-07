bb12499523ac2038fbd04ec584eec81c084f7a3e
Leonardo Fadul <lsffadul@gmail.com>
2023-02-07 17:59:47 -0300

    Entered fully into raw mode

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ void enable_raw_mode()
     // Copy struct before making changes
     struct termios raw = orig_termios;
 
-    /* Local flags: 
+    /* Local mode 
      * ECHO: turn off key printing when typed
      * ICANON: turn off canonical mode
      * ISIG: turn off transmission from Ctrl-C and Ctrl-Z
@@ -32,11 +32,31 @@ void enable_raw_mode()
      */
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
-    /* Input flags
+    /* Input mode
      * IXON: turn off software flow control(Ctrl-S and Ctrl-Q transmission)
      * ICRNL: turn off carriage returns
+     * BRKINT: turn off break condition and avoid SIGINT signal
+     * INPCK: disable parity checking
+     * ISTRIP: disable 8th bit of each input byte to be stripped(set to 0)
      */
-    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+
+    /* Output mode
+     * OPOST: turn off output processing features
+     */
+    raw.c_oflag &= ~(OPOST);
+
+    /* Control mode
+     * CS8: set the character size to 8 bits per byte
+    */
+    raw.c_cflag |= (CS8);
+
+    /* Control characters
+     * VMIN: set min number of bytes of input needed before read() can return
+     * VTIME: set max amount of time to wait before read() returns
+     */
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     
     /* Set terminal attributes
     TCSAFLUSH waits for all pending output to be written to the terminal
@@ -49,14 +69,19 @@ int main(int argc, char** argv)
 {
     enable_raw_mode();
     
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while(true)
     {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+        
         // If c is a control character(nonprintable characters)
         if(iscntrl(c))
-            std::cout << (int)c << "\n";
+            std::cout << (int)c << "\r\n";
         else
-            std::cout << (int)c << " ('" << c << "')\n";
+            std::cout << (int)c << " ('" << c << "')\r\n";
+
+        if(c == 'q')
+            break;
     }
     return 0;
 }

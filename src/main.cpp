302dd6e3d176f1e9ef2de19d6ab6215fe9e8c8a5
Leonardo Fadul <lsffadul@gmail.com>
2023-02-07 17:18:45 -0300

    Disabled raw mode when exiting program

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,36 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// Variable that stores terminal attributes
+struct termios orig_termios;
+
+void disable_raw_mode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enable_raw_mode()
 {
-    // Variable that stores terminal attributes
-    struct termios raw;
+    // Read terminal attributes and put in orig_termios
+    tcgetattr(STDIN_FILENO, &orig_termios);
 
-    // Read terminal attributes and put in raw
-    tcgetattr(STDIN_FILENO, &raw);
+    /* Register disable_raw_mode function to be automatically executed when 
+       program exits */
+    atexit(disable_raw_mode);
+
+    // Copy struct before making changes
+    struct termios raw = orig_termios;
 
     // Local flags: 
     raw.c_lflag &= ~(ECHO);
 
     /* Set terminal attributes
     TCSAFLUSH waits for all pending output to be written to the terminal
-    and discards input that wasn't read */
+     and discards input that wasn't read */
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 
 int main(int argc, char** argv)
 {

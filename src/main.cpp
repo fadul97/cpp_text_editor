bc35bb8276a9b487935eb0b9fc32b2455c862b46
Leonardo Fadul <lsffadul@gmail.com>
2023-02-07 17:12:11 -0300

    Enabled raw mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
+#include <termios.h>
 #include <unistd.h>
+
+void enable_raw_mode()
+{
+    // Variable that stores terminal attributes
+    struct termios raw;
+
+    // Read terminal attributes and put in raw
+    tcgetattr(STDIN_FILENO, &raw);
+
+    // Local flags: 
+    raw.c_lflag &= ~(ECHO);
+
+    /* Set terminal attributes
+    TCSAFLUSH waits for all pending output to be written to the terminal
+    and discards input that wasn't read */
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(int argc, char** argv)
 {
+    enable_raw_mode();
+    
     char c;
-    while(read(STDIN_FILENO, &c, 1) == 1);
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
